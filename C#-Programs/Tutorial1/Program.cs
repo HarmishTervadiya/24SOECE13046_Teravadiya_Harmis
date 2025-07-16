7a8969ebbf57a5242ac318b170a760d6e9150bfc
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-07-16 21:17:41 +0530

    Small Fixes

--- C#-Programs/Tutorial1/Program.cs
+++ C#-Programs/Tutorial1/Program.cs
@@ -1,10 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Name: Teravadiya Harmis");
-Console.WriteLine("DOB: 30/12/2004");
-Console.WriteLine("Address: RKU Hostel");
-Console.WriteLine("City: Rajkot");
-Console.WriteLine("Pincode: 360002");
-Console.WriteLine("State: Gujarat");
-Console.WriteLine("Country: India");
-Console.WriteLine("Email: hteravadiya467@rku.ac.in");
+using System;
+
+public class P1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Name: Teravadiya Harmis");
+        Console.WriteLine("DOB: 30/12/2004");
+        Console.WriteLine("Address: RKU Hostel");
+        Console.WriteLine("City: Rajkot");
+        Console.WriteLine("Pincode: 360002");
+        Console.WriteLine("State: Gujarat");
+        Console.WriteLine("Country: India");
+        Console.WriteLine("Email: hteravadiya467@rku.ac.in");
+    }
+}
