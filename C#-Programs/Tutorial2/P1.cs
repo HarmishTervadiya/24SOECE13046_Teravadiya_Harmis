59856b636bdf05ef7aaed50309010a68252b3940
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-07-17 19:39:10 +0530

    Tutotial2 Completed

---
+++ C#-Programs/Tutorial2/P1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial2
+{
+    internal class P1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length of array");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("Enter " + (i+1) + " element of array");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Array: [");
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                
+            }
+            Console.Write("]");
+
+        }
+    }
+}
---
+++ C#-Programs/Tutorial2/P10.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Tutorial2
+{
+    internal class P10
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of columns:");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix1 = new int[rows, cols];
+            int[,] matrix2 = new int[rows, cols];
+            int[,] sum = new int[rows, cols];
+
+            Console.WriteLine("Enter elements of the first matrix:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("Enter element [" + i + "] [" + j + "]: ");
+                    matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Enter elements of the second matrix:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write("Enter element [" + i + "] [" + j + "]: ");
+                    matrix2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    sum[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            Console.WriteLine("Sum of the matrices:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(sum[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
