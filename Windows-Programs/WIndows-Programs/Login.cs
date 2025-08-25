6d9921395dca048093c0a44b9e5c297bad2f8c2d
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-08-25 18:07:49 +0530

    Learned Updating in database and second approach for query parameters

---
+++ Windows-Programs/WIndows-Programs/Login.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WIndows_Programs
+{
+    public partial class Login : Form
+    {
+
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\24SOECE13046_Harmis_Teravadiya\\Windows-Programs\\WIndows-Programs\\Student_DB.mdf;Integrated Security=True";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO login(email, password) VALUES(@email, @password)", con);
+
+            cmd.Parameters.AddWithValue("@email", textBox1.Text.ToString());
+            cmd.Parameters.AddWithValue("@password", textBox2.Text.ToString());
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("update login set email=@email, password=@password where id=@id", con);
+
+            cmd.Parameters.AddWithValue("@email", textBox1.Text.ToString());
+            cmd.Parameters.AddWithValue("@password", textBox2.Text.ToString());
+            cmd.Parameters.AddWithValue("@id", textBox3.Text.ToString());
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Updated successfully");
+        }
+    }
+}
--- Windows-Programs/WIndows-Programs/Program.cs
+++ Windows-Programs/WIndows-Programs/Program.cs
@@ -16,7 +16,7 @@ namespace WIndows_Programs
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
