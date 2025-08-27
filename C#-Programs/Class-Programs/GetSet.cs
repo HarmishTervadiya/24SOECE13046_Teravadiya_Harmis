63ab2555705420752e4f33f598b9304b5933c9ef
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-08-27 13:50:33 +0530

    Implemented the getter and setter

---
+++ C#-Programs/Class-Programs/GetSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Programs
+{
+    internal class GetSet
+    {
+        class Student
+        {
+            private int id;
+            public int getId()
+            {
+                return id;
+            }
+
+            public void setId(int id)
+            {
+                this.id = id;
+            }
+
+            public int ID
+            {
+                get { return id; }
+                set { id = value; }
+            }
+
+        }
+
+        class PrototypeDemo
+        {
+            public static void Main()
+            {
+                Student obj = new Student();
+                Console.WriteLine(obj.ID);
+                Console.WriteLine(obj.getId());
+            }
+        }
+    }
+}
