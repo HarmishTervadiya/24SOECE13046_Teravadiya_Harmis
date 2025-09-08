40487c0f5ea178b0b37b2245403d196c6ebaebe6
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-09-08 17:26:28 +0530

    Binded data grid with db

---
+++ Windows-Programs/Tutorial9/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Tutorial9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.View_BTN = new System.Windows.Forms.Button();
+            this.Insert_BTN = new System.Windows.Forms.Button();
+            this.Find = new System.Windows.Forms.Button();
+            this.Update_BTN = new System.Windows.Forms.Button();
+            this.Delete_BTN = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.courseDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dobDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mobDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.table1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet1 = new Tutorial9.Database1DataSet1();
+            this.database1DataSet = new Tutorial9.Database1DataSet();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableTableAdapter = new Tutorial9.Database1DataSetTableAdapters.TableTableAdapter();
+            this.table1TableAdapter = new Tutorial9.Database1DataSet1TableAdapters.Table1TableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "course";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "dob";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(184, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(184, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "mobile";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(275, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(275, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(275, 170);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(275, 208);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // View_BTN
+            // 
+            this.View_BTN.Location = new System.Drawing.Point(50, 255);
+            this.View_BTN.Name = "View_BTN";
+            this.View_BTN.Size = new System.Drawing.Size(75, 23);
+            this.View_BTN.TabIndex = 10;
+            this.View_BTN.Text = "View";
+            this.View_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Insert_BTN
+            // 
+            this.Insert_BTN.Location = new System.Drawing.Point(166, 255);
+            this.Insert_BTN.Name = "Insert_BTN";
+            this.Insert_BTN.Size = new System.Drawing.Size(75, 23);
+            this.Insert_BTN.TabIndex = 11;
+            this.Insert_BTN.Text = "Insert";
+            this.Insert_BTN.UseVisualStyleBackColor = true;
+            this.Insert_BTN.Click += new System.EventHandler(this.Insert_BTN_Click);
+            // 
+            // Find
+            // 
+            this.Find.Location = new System.Drawing.Point(275, 255);
+            this.Find.Name = "Find";
+            this.Find.Size = new System.Drawing.Size(75, 23);
+            this.Find.TabIndex = 12;
+            this.Find.Text = "Find";
+            this.Find.UseVisualStyleBackColor = true;
+            this.Find.Click += new System.EventHandler(this.Find_Click);
+            // 
+            // Update_BTN
+            // 
+            this.Update_BTN.Location = new System.Drawing.Point(380, 255);
+            this.Update_BTN.Name = "Update_BTN";
+            this.Update_BTN.Size = new System.Drawing.Size(75, 23);
+            this.Update_BTN.TabIndex = 13;
+            this.Update_BTN.Text = "Update";
+            this.Update_BTN.UseVisualStyleBackColor = true;
+            this.Update_BTN.Click += new System.EventHandler(this.Update_BTN_Click);
+            // 
+            // Delete_BTN
+            // 
+            this.Delete_BTN.Location = new System.Drawing.Point(480, 255);
+            this.Delete_BTN.Name = "Delete_BTN";
+            this.Delete_BTN.Size = new System.Drawing.Size(75, 23);
+            this.Delete_BTN.TabIndex = 14;
+            this.Delete_BTN.Text = "Delete";
+            this.Delete_BTN.UseVisualStyleBackColor = true;
+            this.Delete_BTN.Click += new System.EventHandler(this.Delete_BTN_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.courseDataGridViewTextBoxColumn,
+            this.dobDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.mobDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.table1BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 301);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(807, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // courseDataGridViewTextBoxColumn
+            // 
+            this.courseDataGridViewTextBoxColumn.DataPropertyName = "course";
+            this.courseDataGridViewTextBoxColumn.HeaderText = "course";
+            this.courseDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.courseDataGridViewTextBoxColumn.Name = "courseDataGridViewTextBoxColumn";
+            this.courseDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // dobDataGridViewTextBoxColumn
+            // 
+            this.dobDataGridViewTextBoxColumn.DataPropertyName = "dob";
+            this.dobDataGridViewTextBoxColumn.HeaderText = "dob";
+            this.dobDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.dobDataGridViewTextBoxColumn.Name = "dobDataGridViewTextBoxColumn";
+            this.dobDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "address";
+            this.addressDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // mobDataGridViewTextBoxColumn
+            // 
+            this.mobDataGridViewTextBoxColumn.DataPropertyName = "mob";
+            this.mobDataGridViewTextBoxColumn.HeaderText = "mob";
+            this.mobDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.mobDataGridViewTextBoxColumn.Name = "mobDataGridViewTextBoxColumn";
+            this.mobDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // table1BindingSource
+            // 
+            this.table1BindingSource.DataMember = "Table1";
+            this.table1BindingSource.DataSource = this.database1DataSet1;
+            // 
+            // database1DataSet1
+            // 
+            this.database1DataSet1.DataSetName = "Database1DataSet1";
+            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataSource = this.database1DataSet1;
+            this.tableBindingSource.Position = 0;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // table1TableAdapter
+            // 
+            this.table1TableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Delete_BTN);
+            this.Controls.Add(this.Update_BTN);
+            this.Controls.Add(this.Find);
+            this.Controls.Add(this.Insert_BTN);
+            this.Controls.Add(this.View_BTN);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button View_BTN;
+        private System.Windows.Forms.Button Insert_BTN;
+        private System.Windows.Forms.Button Find;
+        private System.Windows.Forms.Button Update_BTN;
+        private System.Windows.Forms.Button Delete_BTN;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Database1DataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn courseDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dobDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mobDataGridViewTextBoxColumn;
+        private Database1DataSet1 database1DataSet1;
+        private System.Windows.Forms.BindingSource table1BindingSource;
+        private Database1DataSet1TableAdapters.Table1TableAdapter table1TableAdapter;
+
+
+    }
+}
+
---
+++ Windows-Programs/Tutorial9/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial9
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\24SOECE13046_Harmis_Teravadiya\\Windows-Programs\\Tutorial9\\Database1.mdf;Integrated Security=True";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.table1TableAdapter.Fill(this.database1DataSet1.Table1);
+            dataGridView1.CellClick += dataGridView1_CellClick;
+
+        }
+
+        private void Insert_BTN_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Table1 (name, course, dob, address, mob) VALUES (@name, @course, @dob, @address, @mob)", con);
+
+            cmd.Parameters.AddWithValue("@name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@course", textBox2.Text);
+            cmd.Parameters.AddWithValue("@dob", textBox3.Text);
+            cmd.Parameters.AddWithValue("@address", textBox4.Text);
+            cmd.Parameters.AddWithValue("@mob", Convert.ToInt32(textBox5.Text));
+
+            
+            cmd.ExecuteNonQuery();
+            
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Table1", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+
+            MessageBox.Show("Added successfully");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Delete_BTN_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM Table1 where id=@id", con);
+            cmd.Parameters.AddWithValue("@id",dataGridView1.SelectedCells[0].Value);
+
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Table1", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+            
+        }
+
+        private void Update_BTN_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Table1 SET name=@name, course=@course, dob=@dob, address=@address, mob=@mob WHERE id=@id", con);
+
+            cmd.Parameters.AddWithValue("@name", textBox1.Text);
+            cmd.Parameters.AddWithValue("@course", textBox2.Text);
+            cmd.Parameters.AddWithValue("@dob", textBox3.Text);
+            cmd.Parameters.AddWithValue("@address", textBox4.Text);
+                cmd.Parameters.AddWithValue("@mob", Convert.ToInt32(textBox5.Text));
+            cmd.Parameters.AddWithValue("@id", dataGridView1.SelectedCells[0].Value);
+
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Table1", con);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void Find_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
