a8d58ed11c9bf27c8033b8e5e9a27097a204641a
HarmishTervadiya <harmishtervadiya@gmail.com>
2025-09-16 21:42:53 +0530

    Completed Tutorial 10

---
+++ Windows-Programs/Tutorial10/Ex6.Designer.cs
@@ -0,0 +1,128 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tutorial10
+{
+    partial class Ex6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private TextBox txtFrom;
+        private TextBox txtTo;
+        private TextBox txtMessage;
+        private Button btnSend;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.Location = new System.Drawing.Point(20, 30);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(50, 20);
+            this.lblFrom.TabIndex = 0;
+            this.lblFrom.Text = "From:";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(80, 28);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.ReadOnly = true;
+            this.txtFrom.Size = new System.Drawing.Size(280, 22);
+            this.txtFrom.TabIndex = 1;
+            this.txtFrom.Text = "hteravadiya467@rku.ac.in";
+            // 
+            // lblTo
+            // 
+            this.lblTo.Location = new System.Drawing.Point(20, 60);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(50, 20);
+            this.lblTo.TabIndex = 2;
+            this.lblTo.Text = "To:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(80, 58);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(280, 22);
+            this.txtTo.TabIndex = 3;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(20, 90);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(60, 20);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "Message:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(80, 88);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(280, 100);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(160, 210);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(80, 30);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click_1);
+            // 
+            // Ex6
+            // 
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Ex6";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Email Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblFrom;
+        private Label lblTo;
+        private Label lblMessage;
+    }
+}
