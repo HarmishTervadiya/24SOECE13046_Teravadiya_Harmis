e8e53f303270a3857a16555be0e71e1a7e52251b
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-01-27 14:41:05 +0530

    .Net core basic navigation

---
+++ .NetCore/Tanna-sir/NetCoreWebApp Example/Controllers/LoanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebApp_Example.Controllers
+{
+    public class LoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult UpdateApplication()
+        {
+            return View();
+        }
+
+        public IActionResult DiscardApplication()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAll()
+        {
+            return View();
+        }
+    }
+}
