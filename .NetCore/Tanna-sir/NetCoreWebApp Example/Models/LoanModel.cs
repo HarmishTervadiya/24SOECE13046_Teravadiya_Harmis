49a512cc79ef8bb21b90d90323757cb03af4368c
HarmishTervadiya <harmishtervadiya@gmail.com>
2026-02-10 14:30:03 +0530

    defining loan model

---
+++ .NetCore/Tanna-sir/NetCoreWebApp Example/Models/LoanModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreWebApp_Example.Models
+{
+    public class LoanModel
+    {
+       
+
+        [Required(ErrorMessage = "*")]
+        public int LoanNo { get; set; }
+        [Required(ErrorMessage = "*")]
+
+        public int AccountNo { get; set; }
+        [Required(ErrorMessage = "*")]
+
+
+        public int Amount { get; set; }
+        [Required(ErrorMessage = "*")]
+
+        public string LoanType { get; set; }
+        [Required(ErrorMessage = "*")]
+
+        public string LoanCategory { get; set; }
+        [Required(ErrorMessage = "*")]
+
+        public string CurrentAddress { get; set; }
+        [Required(ErrorMessage = "*")]
+
+        public string LoanRemarks { get; set; }
+
+    }
+}
